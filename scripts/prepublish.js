847f1d13f44e48960fa14b8267793fa0e27000f2
Tetsuya Mori <tetsuya.mori@kidsstar.co.jp>
2017-01-26 10:15:44 +0900

    Fix prepublish script for copy

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,12 +1,14 @@
 var path = require('path');
 var ncp = require('ncp');
+var mkdirp = require('mkdirp');
 var fs = require('fs');
 
 // Paths
 var src_base = path.join(__dirname, '..', '..', '..', 'Assets', 'Packages');
 var dst_base = path.join(__dirname, '..', 'src');
 
-var exists = function(path) {
+// Function: Check file or directory exists
+var isExists = function(path) {
   try {
     fs.statSync(path);
     return true;
@@ -16,29 +18,65 @@ var exists = function(path) {
   return false;
 };
 
-// Target files and directories in the destination directory
-fs.readdir(dst_base, function(err, files) {
+// Function: Check path is directory or not
+var isDirectory = function(path) {
+  try {
+    return fs.statSync(path).isDirectory();
+  } catch (err) {
+    return false;
+  }
+  return false;
+};
+
+// Function: Copy files recursive
+var copyFile = function(path_src, path_dst) {
+  ncp(path_src, path_dst, function(err) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+};
+
+// Read 'package.json'
+fs.readFile('package.json', { 'encoding': 'utf8' }, function(err, data) {
+  // Exit: If occurs read error
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  if (!files || 0 == files.length) {
-    console.error('Any files or directories does not exists in "' + dst_base + '".');
+
+  var package_json = JSON.parse(data);
+
+  // Exit: If 'files' node does not exists or empty
+  if (!package_json || !package_json.files || 0 == package_json.files.length) {
+    console.error('Could not found file / directory list in "package.json"');
     process.exit(1);
   }
 
-  files.forEach(function(file) {
+  // Each 'files' node entry
+  package_json.files.forEach(function(file) {
     var src = path.join(src_base, file);
     var dst = path.join(dst_base, file);
-    if (!exists(src)) {
-      console.error('Source files or directories does not exists: "' + src + '"');
+    // Exit: If source file does not exists
+    if (!isExists(src)) {
+      console.error('Source file / directory could not found: ["' + src + '"]');
       process.exit(1);
     }
-    ncp(src, dst, function(err) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-    })
+
+    // Copy file
+    // Create directory if needed
+    if (isDirectory(src) && !isExists(dst)) {
+      mkdirp(dst, function(err) {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+
+        copyFile(src, dst);
+      });
+    } else {
+      copyFile(src, dst);
+    }
   });
 });
