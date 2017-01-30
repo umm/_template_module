9f3cd85eb9ea121510a1e8266db84fa22fb25447
Tetsuya Mori <tetsuya.mori@kidsstar.co.jp>
2017-01-30 20:53:56 +0900

    Add files for Tests

---
+++ test/gulpfile.babel.js
@@ -0,0 +1,58 @@
+import 'babel-polyfill';
+import gulp from 'gulp';
+import run from 'run-sequence';
+import unity from 'gulp-unity';
+import xml2js from 'xml2js';
+import fs from 'fs';
+import path from 'path';
+
+/// Dev target pattern
+var dev = 'EventsTests';
+
+/// Results file
+var target = path.join(__dirname, 'EditorTestResults.xml');
+
+gulp.task('default', (callback) => {
+  fs.unlink(target, () => {
+    run('unity-all', () => {
+      unity.debug_test_results(target);
+      callback();
+    });
+  });
+});
+
+gulp.task('dev', (callback) => {
+  fs.unlink(target, () => {
+    run('unity-dev', () => {
+      var target = path.join(__dirname, 'EditorTestResults.xml');
+      unity.debug_test_results(target);
+      callback();
+    });
+  });
+});
+
+// Run unity tests
+gulp.task('unity-all', () => {
+  return gulp.src('./package.json')
+    .pipe(unity({
+      args: ['-runEditorTests'],
+      debug: (v) => {
+        v.debug([
+          { pattern: /.*/, color: 'yellow' }
+        ])
+      }
+    }));
+});
+
+// Run unity tests
+gulp.task('unity-dev', () => {
+  return gulp.src('./package.json')
+    .pipe(unity({
+      args: ['-runEditorTests', '-editorTestsFilter', dev],
+      debug: (v) => {
+        v.debug([
+          { pattern: /.*/, color: 'yellow' }
+        ])
+      }
+    }));
+});
