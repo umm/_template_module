b81938d6effc9ad41b60c70b20cbee1f87d0b2c9
Tetsuya Mori <monry84@gmail.com>
2017-07-03 13:34:31 +0900

    :beetle: アセットのパスをシンプルにする

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,25 +2,23 @@ var mkdirp = require('mkdirp');
 var path = require('path');
 var ncp = require('ncp');
 
-// Paths
-var src = path.join(__dirname, '..', 'src');
-if ('node_modules' == path.basename(path.dirname(process.cwd()))) {
-  // Normal installation
-  var dir = path.join(__dirname, '..', '..', '..', 'Assets', 'Packages');
-} else {
-  // Development installation
-  var dir = path.join(__dirname, '..', 'Assets');
+// Finish if development installation
+if ('node_modules' != path.basename(path.dirname(process.cwd()))) {
+  return;
 }
+// Paths
+var source = path.join(__dirname, '..', 'Assets');
+var destination = path.join(__dirname, '..', '..', '..', 'Assets', 'Packages');
 
 // Create folder if missing
-mkdirp(dir, function(err) {
+mkdirp(destination, function(err) {
   if (err) {
-    console.error(err)
+    console.error(err);
     process.exit(1);
   }
 
   // Copy files
-  ncp(src, dir, function(err) {
+  ncp(source, destination, function(err) {
     if (err) {
       console.error(err);
       process.exit(1);
