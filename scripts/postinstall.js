a60c6be6fe87e3321e19459f96ebb285ffeabef5
Tetsuya Mori <monry84@gmail.com>
2017-07-05 00:03:06 +0900

    :sparkles: インストール後起動スクリプトの改良
    * パスの解決方法を可読性高くする
    * Scoped なパッケージをサポート
    * コメントを日本語化

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,22 +2,32 @@ var mkdirp = require('mkdirp');
 var path = require('path');
 var ncp = require('ncp');
 
-// Finish if development installation
-if ('node_modules' != path.basename(path.dirname(process.cwd()))) {
+var script_directory = __dirname;
+var has_scope = false;
+if (/^@/.test(path.basename(path.resolve(script_directory, '../../')))) {
+  // 二つ親のディレクトリが @ で始まる場合は Scoped なパッケージと見なす
+  // XXX: package.json の中身を読むとヨサソウ
+  has_scope = true;
+}
+
+if ('node_modules' != path.basename(path.resolve(script_directory, (has_scope ? '../' : '') + '../../'))) {
+  // 開発インストールの場合無視する
   return;
 }
-// Paths
-var source = path.join(__dirname, '..', 'Assets');
-var destination = path.join(__dirname, '..', '..', '..', 'Assets', 'Packages');
 
-// Create folder if missing
+// スクリプトの存在するディレクトリから見たパス
+var source = path.resolve(script_directory, '../Assets');
+var destination = path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Modules');
+
+// 宛先ディレクトリを作る (mkdir -p)
 mkdirp(destination, function(err) {
   if (err) {
-    console.error(err);
+    console.error(err)
     process.exit(1);
   }
 
-  // Copy files
+  // ファイルを再帰的にコピーする
+  // NOTE: ココは rsync --delete とかにするかも
   ncp(source, destination, function(err) {
     if (err) {
       console.error(err);
